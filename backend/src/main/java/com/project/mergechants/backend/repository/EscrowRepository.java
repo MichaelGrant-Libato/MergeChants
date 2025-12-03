0b2ebc553939cb67d987888e134726ac010696a0
MichaelGrant-Libato <michaelgrantlibato7@gmail.com>
2025-12-03 19:56:58 +0800

    fix: remove extra backend folder

--- backend/src/main/java/com/project/mergechants/backend/repository/EscrowRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.mergechants.backend.repository;
-
-import com.project.mergechants.backend.entity.EscrowEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface EscrowRepository extends JpaRepository<EscrowEntity, Long> {
-    List<EscrowEntity> findByBuyerId(String buyerId);
-}
